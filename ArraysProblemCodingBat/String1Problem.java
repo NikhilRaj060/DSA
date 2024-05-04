72fe0e112575ec6bbb07cb53648a565278da3177
NikhilRaj060 <nikhilraj2061@gmail.com>
2024-05-05 00:50:22 +0530

    Finished String 1 probelm.

--- ArraysProblemCodingBat/String1Problem.java
+++ ArraysProblemCodingBat/String1Problem.java
@@ -341,7 +341,6 @@ public boolean hasBad(String str) {
   return str.length() >= 3 ? str.substring(0,3).equals("bad") || str.substring(1,4).equals("bad") : false;
 }
 
-
  //21th
 
 /*
@@ -363,80 +362,201 @@ atFirst("h") → "h@"
 
 /*
  * 
+Given 2 strings, a and b, return a new string made of the first char of a and the last char of b, so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+
+
+lastChars("last", "chars") → "ls"
+lastChars("yo", "java") → "ya"
+lastChars("hi", "") → "h@"
  */
+
+ public String lastChars(String a, String b) {
+  return a.length() < 1 && b.length() < 1 ? "@@" : a.length() >= 1 && b.length() >= 1 ? a.substring(0,1) + b.substring(b.length()-1,b.length()) : a.length() < 1 ? "@" + b.substring(b.length()-1,b.length()) : a.substring(0,1) + "@"; 
+}
 
 
  //23th
 
 /*
  * 
+Given two strings, append them together (known as "concatenation") and return the result. However, if the concatenation creates a double-char, then omit one of the chars, so "abc" and "cat" yields "abcat".
+
+
+conCat("abc", "cat") → "abcat"
+conCat("dog", "cat") → "dogcat"
+conCat("abc", "") → "abc"
  */
+
+public String conCat(String a, String b) {
+  return a.length() >= 1 && b.length() >= 1 && b.substring(0,1).equals(a.substring(a.length()-1)) ? a + b.substring(1,b.length()) : a+b;
+}
+
 
  //24th
 
 /*
  * 
+Given a string of any length, return a new string where the last 2 chars, if present, are swapped, so "coding" yields "codign".
+
+
+lastTwo("coding") → "codign"
+lastTwo("cat") → "cta"
+lastTwo("ab") → "ba"
  */
+
+public String lastTwo(String str) {
+  return str.length() < 2 ? str : str.substring(0,str.length()-2) +  str.substring(str.length()-1) + str.substring(str.length()-2,str.length()-1);
+}
 
 
  //25th
 
 /*
  * 
+Given a string, if the string begins with "red" or "blue" return that color string, otherwise return the empty string.
+
+
+seeColor("redxx") → "red"
+seeColor("xxred") → ""
+seeColor("blueTimes") → "blue"
  */
+
+public String seeColor(String str) {
+  return str.length() >= 3 ? str.substring(0,3).equals("red") ? "red" : str.length() >= 4 && str.substring(0,4).equals("blue") ? "blue" : "" : "";
+}
 
 
  //26th
 
 /*
  * 
+Given a string, return true if the first 2 chars in the string also appear at the end of the string, such as with "edited".
+
+
+frontAgain("edited") → true
+frontAgain("edit") → false
+frontAgain("ed") → true
  */
+
+public boolean frontAgain(String str) {
+  return str.length() >= 2 && str.substring(0,2).equals(str.substring(str.length()-2,str.length()));
+}
 
 
 //27th
 
 /*
  * 
+Given two strings, append them together (known as "concatenation") and return the result. However, if the strings are different lengths, omit chars from the longer string so it is the same length as the shorter string. So "Hello" and "Hi" yield "loHi". The strings may be any length.
+
+
+minCat("Hello", "Hi") → "loHi"
+minCat("Hello", "java") → "ellojava"
+minCat("java", "Hello") → "javaello"
  */
+
+ public String minCat(String a, String b) {
+  return a.length() > b.length() ? a.substring(a.length()-b.length()) + b.substring(0) : a.substring(0) + b.substring(b.length()-a.length());
+}
 
 
  //28th
 
 /*
  * 
+Given a string, return a new string made of 3 copies of the first 2 chars of the original string. The string may be any length. If there are fewer than 2 chars, use whatever is there.
+
+
+extraFront("Hello") → "HeHeHe"
+extraFront("ab") → "ababab"
+extraFront("H") → "HHH"
  */
+
+ public String extraFront(String str) {
+  return str.length() > 2 ? str.substring(0,2) + str.substring(0,2) + str.substring(0,2) : str +  str + str;
+}
 
 
  //29th
 
 /*
- * 
+ *
+Given a string, if a length 2 substring appears at both its beginning and end, return a string without the substring at the beginning, so "HelloHe" yields "lloHe". The substring may overlap with itself, so "Hi" yields "". Otherwise, return the original string unchanged.
+
+
+without2("HelloHe") → "lloHe"
+without2("HelloHi") → "HelloHi"
+without2("Hi") → ""
  */
+
+ public String without2(String str) {
+  return str.length() >= 2 && str.substring(0,2).equals(str.substring(str.length()-2)) ? str.substring(2) : str;
+}
 
 
  //30th
 
 /*
  * 
+Given a string, return a version without the first 2 chars. Except keep the first char if it is 'a' and keep the second char if it is 'b'. The string may be any length. Harder than it looks.
+
+
+deFront("Hello") → "llo"
+deFront("java") → "va"
+deFront("away") → "aay"
  */
+public String deFront(String str) {    
+  return str.substring(0,1).equals("a") && str.substring(1,2).equals("b") ? str : str.substring(0,1).equals("a") ? str.substring(0,1) + str.substring(2) : str.substring(1,2).equals("b") ? str.substring(1,2) + str.substring(2) : str.substring(2);
+}
 
 
  //31th
 
 /*
  * 
+Given a string and a second "word" string, we'll say that the word matches the string if it appears at the front of the string, except its first char does not need to match exactly. On a match, return the front of the string, or otherwise return the empty string. So, so with the string "hippo" the word "hi" returns "hi" and "xip" returns "hip". The word will be at least length 1.
+
+
+startWord("hippo", "hi") → "hi"
+startWord("hippo", "xip") → "hip"
+startWord("hippo", "i") → "h"
  */
 
+ public String startWord(String str, String word) {
+  return str.length() >= word.length() && str.substring(1,word.length()).equals(word.substring(1,word.length())) ? str.substring(0,word.length()) : "";
+}
 
  //32th
 
 /*
- * 
+ *
+Given a string, if the first or last chars are 'x', return the string without those 'x' chars, and otherwise return the string unchanged.
+
+
+withoutX("xHix") → "Hi"
+withoutX("xHi") → "Hi"
+withoutX("Hxix") → "Hxi"
  */
+
+public String withoutX(String str) {
+  if (str.length() <= 0) return str;
+  return str.substring(0,1).equals("x") && str.substring(str.length()-1).equals("x") && str.length() > 1 ? str.substring(1,str.length()-1) : str.substring(0,1).equals("x") ? str.substring(1) : str.substring(str.length()-1).equals("x") ? str.substring(0,str.length()-1) : str;
+}
 
 
  //33th
 
 /*
- * 
+ *
+Given a string, if one or both of the first 2 chars is 'x', return the string without those 'x' chars, and otherwise return the string unchanged. This is a little harder than it looks.
+
+
+withoutX2("xHi") → "Hi"
+withoutX2("Hxi") → "Hi"
+withoutX2("Hi") → "Hi"
  */
+
+ public String withoutX2(String str) {
+  if (str.length() <= 1) return "";
+  return str.substring(0,1).equals("x") && str.substring(1,2).equals("x") ? str.substring(2) : str.substring(0,1).equals("x") ? str.substring(1) : str.substring(1,2).equals("x") ? str.substring(0,1) + str.substring(2) : str;
+}
